7fb0fe80a90c7c8183973c3e51184cba6101ba53
Omen <Divyanshu>
2025-05-01 22:43:53 +0530

    cors error resolved

--- backend/main.py
+++ backend/main.py
@@ -77,16 +77,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Doodle Recognition API", lifespan=lifespan)
 
-# List of allowed origins
-origins = [
-    "http://localhost",              # Allow local dev frontend
-    "http://localhost:3000",        # e.g., React dev server
-    "https://doodle-classification-system.onrender.com",    # Your production frontend domain
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,          # List of allowed origins
+    allow_origins=["*"],          # List of allowed origins
     allow_credentials=True,
     allow_methods=["*"],            # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],            # Allow all headers
